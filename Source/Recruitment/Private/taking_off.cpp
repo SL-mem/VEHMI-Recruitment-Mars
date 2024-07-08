7df3a06636462c5ade11922aa84aa7ad132f802c
SL-mem <salmanm.job@gmail.com>
2024-07-08 17:16:44 +0200

    Structured blade rotation with a function and keyboard movementof the cubic target

---
+++ Source/Recruitment/Private/taking_off.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "taking_off.h"
+
+// Sets default values
+Ataking_off::Ataking_off()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void Ataking_off::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void Ataking_off::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
